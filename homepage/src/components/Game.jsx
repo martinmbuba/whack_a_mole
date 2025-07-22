2679b89e1c1b6c593982192089151f00f1359f75
JxAmani <jeremyamani336@gmail.com>
2025-07-22 12:19:25 +0300

    WM-32 <Implementing ame button>

---
+++ homepage/src/components/Game.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div>
+      {/* Game logic will go here later */}
+      <h2>
+        Are you ready to play?
+
+      </h2>
+    </div>
+  );
+}
+
+export default Game;
