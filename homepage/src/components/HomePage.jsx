f237b0c316a820cbd339d11426679b5f8fb1a1d8
JxAmani <jeremyamani336@gmail.com>
2025-07-22 11:33:52 +0300

    "WM-29 <Adding instruction bar>

--- homepage/src/components/HomePage.jsx
+++ homepage/src/components/HomePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import InstructionBar from './InstructionBar';
+import InstructionBar from './nstructionBar';
 import Login from './Login';
 import Register from './Register';
 
 function HomePage() {
+  return (
+    <div style={styles.container}>
+      <InstructionBar />
+      <h1>Welcome to Whack-a-Mole</h1>
+    
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px',
+  },
 
 };
 
