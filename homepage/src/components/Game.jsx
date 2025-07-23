02a592a1fcc06d4c6e94d1861f8b46b25c124dbb
Umurowoto <umurowoto132@gmail.com>
2025-07-23 23:22:51 +0300

    WM-51 mole appearance logic

--- homepage/src/components/Game.jsx
+++ homepage/src/components/Game.jsx
@@ -1,25 +1,34 @@
+import { useEffect, useState } from "react";
 import "../App.css";
 
 import Hole from '../assets/Hole.jpeg';
-import mole from '../assets/mole.jpeg';
+import mole from '../assets/mole.png';
 
 function Game() {
+  const [moles, setMoles] = useState(new Array(9).fill(false));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * moles.length);
+      const newMoles = new Array(9).fill(false); // Only show 1 mole at a time
+      newMoles[randomIndex] = true;
+      setMoles(newMoles);
+    }, 1);
+
+    return () => clearInterval(interval); // cleanup
+  }, [moles.length]);
+
   return (
-    <div className='grid'>
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
-      <img src={Hole} alt="Hole" />
+    <div className="grid">
+      {moles.map((isMole, index) => (
+        <img
+          key={index}
+          src={isMole ? mole : Hole}
+          alt={isMole ? "Mole" : "Hole"}
+        />
+      ))}
     </div>
   );
 }
-
-
-
 
 export default Game;
